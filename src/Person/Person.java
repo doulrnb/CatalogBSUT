bf61478f1d14c3cbc4d21e658a68aab97a54ffe6
whitewear <wendbeat@yandex.by>
2021-03-18 15:17:36 +0300

    add class Person

---
+++ src/Person/Person.java
@@ -0,0 +1,15 @@
+package Person;
+
+public class Person {
+   private String name;
+   private String surname;
+   private String patronymic;
+   private String department;
+   int office;
+   int phone;
+
+   public void filtre() {
+
+   }    //filtre()
+
+}   //class Person()
